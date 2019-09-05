7c37648f0b6807d40ee4ea7bb3a1586900926a0b
Pavel Smirnov <paulsmirnov@users.noreply.github.com>
2019-09-05 19:27:02 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='rdchiral',
+    version='0.5.0',
+    description='Wrapper for RDKit\'s RunReactants to improve stereochemistry handling',
+    author='Connor Coley',
+    author_email='ccoley@mit.edu',
+    license='MIT',
+    url='https://github.com/connorcoley/rdchiral',
+    packages=['rdchiral'],
+)
